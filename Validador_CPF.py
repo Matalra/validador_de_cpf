30ae24479dc2b7a631573c56323f7ae691a93021
Matalra <main_matheusferreira@hotmail.com>
2021-10-25 22:24:52 -0300

    Added random valid CPF generator

--- Validador_CPF.py
+++ Validador_CPF.py
@@ -1,7 +1,13 @@
-def get_cpf():
-    cpf = input('Digite o seu CPF, apenas os números >>> ')
-    print()
-    return cpf
+from random import randint
+
+def get_cpf(n = None):
+    generating = False
+    if not n:
+        cpf = input('Digite o seu CPF, apenas os números >>> ')
+    else:
+        cpf = n
+        generating = True
+    return cpf, generating
 
 
 # Retira dois ultimos números do CPF e verifica por caracteres invalidos
@@ -9,10 +15,12 @@ def prepare_cpf(cpf):
     if len(cpf) != 11:
             print(f'O CPF foi digitado incorretamente, verifique os números e tente novamente...')
             validador_de_cpf()
+    
     for i in cpf:
         if not i.isnumeric():
             print(f'"{i}" Não é número, digite o CPF novamente, por favor...')
             validador_de_cpf()
+    
     if not int(cpf):
         print(f'O CPF foi digitado incorretamente, verifique os números e tente novamente...')
         validador_de_cpf()
@@ -39,17 +47,30 @@ def get_digits(pre_worked_cpf):
     return creat_digits(total)
 
 
-def validador_de_cpf():
-    cpf = get_cpf()
+def validador_de_cpf(n = None):
+    cpf, generating = get_cpf(n)
     worked_cpf = prepare_cpf(cpf)
 
     while len(worked_cpf) != len(cpf):
         digito = get_digits(worked_cpf)
         worked_cpf += str(digito)
 
-    print(f"O CPF {cpf} é valido!" if cpf == worked_cpf else f"O CPF {cpf} invalido")
+    if cpf == worked_cpf:
+        print()
+        print(f"O CPF {cpf} é valido!")
+        return cpf
+
+    elif generating:
+        return False
+   
+    else:
+        print(f"O CPF {cpf} é invalido!")
 
 
-###################################################
-
-validador_de_cpf()
+# Gerador de CPF aleatórios e validos
+def gera_cpf():
+    while True:
+        rand_cpf = str(randint(11111111211,99999999999))
+        cpf_valido = validador_de_cpf(rand_cpf)
+        if cpf_valido:
+            return cpf_valido
